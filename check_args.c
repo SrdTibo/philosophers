6802bae11d74d185bc68adff387b8d8379f0c46c
Thibault Serdet <tserdet@k0r3p5.42mulhouse.fr>
2023-04-05 14:41:25 +0200

    d

--- check_args.c
+++ check_args.c
@@ -6,17 +6,17 @@
 /*   By: tserdet <tserdet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 13:15:50 by tserdet           #+#    #+#             */
-/*   Updated: 2023/04/05 14:37:44 by tserdet          ###   ########.fr       */
+/*   Updated: 2023/04/05 14:41:04 by tserdet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int convert_args(int argc, char **argv, t_args *args)
+int convert_args(char **argv, t_args *args)
 {
 	args->nmb_philos = atoi(argv[1]);
 	args->ttd = atoi(argv[2]);
-	args->tte = atoi(agrv[3]);
+	args->tte = atoi(argv[3]);
 	args->tts = atoi(argv[4]);
 	if (args->nmb_of_eat != -1)
 		args->nmb_of_eat = atoi(argv[5]);
@@ -25,11 +25,10 @@ int convert_args(int argc, char **argv, t_args *args)
 
 int	check_args(int argc, char **argv, t_args *args)
 {
-	(void) argv;
 	if (argc < 5 || argc > 6)
 		return (1);
 	if (argc == 5)
 		args->nmb_of_eat = -1;
-	convert_args(argc, argv, args);
+	convert_args(argv, args);
 	return (0);
 }
