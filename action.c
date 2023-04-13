e52757e60d7685fa7e966145165d84c62af84db9
Thibault Serdet <tserdet@k0r3p5.42mulhouse.fr>
2023-04-13 14:36:45 +0200

    time of day

--- action.c
+++ action.c
@@ -6,7 +6,7 @@
 /*   By: tserdet <tserdet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 13:48:27 by tserdet           #+#    #+#             */
-/*   Updated: 2023/04/13 14:32:23 by tserdet          ###   ########.fr       */
+/*   Updated: 2023/04/13 14:36:24 by tserdet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	thinking(int x, t_gen *gen)
 {
 	pthread_mutex_lock(&gen->write);
-	printf("%dms Philo %d is thinking\n", get_chrono(gen), x);
+	printf("%dms philo %d is thinking\n", get_chrono(gen), x);
 	pthread_mutex_unlock(&gen->write);
 }
