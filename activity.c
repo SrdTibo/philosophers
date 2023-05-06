82211367e46c1d9d220a3fa1f3497dfe662c0a55
Thibault Serdet <tserdet@k0r3p5.42mulhouse.fr>
2023-05-06 23:14:12 +0200

    ALL OK , no norm

--- activity.c
+++ activity.c
@@ -6,7 +6,7 @@
 /*   By: tserdet <tserdet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 10:47:02 by tserdet           #+#    #+#             */
-/*   Updated: 2023/05/06 20:51:06 by tserdet          ###   ########.fr       */
+/*   Updated: 2023/05/06 23:13:55 by tserdet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ void	*activity(void *data)
 	if (philos->nb_eat != 2147483647)
 		philos->nb_eat += 1;
 	ft_usleep(philos->args->tte, philos->begin_all);
-	// printf("\033[0;31m%dms Philo %d eated %d times (%ldms)\033[0m\n",get_chrono(philos->begin_all), philos->id  + 1, philos->nb_eat, philos->log_eat);
 	pthread_mutex_unlock(&philos->f_c);
 	pthread_mutex_unlock(philos->f_l);
 	is_sleeping(philos->id, *philos->ptr_write, philos->begin_all);
@@ -68,14 +67,11 @@ void	*threads(void *data)
 int launch_threads(t_args *args, t_gen *gen, t_all *all)
 {
 	int i;
-	// long int	time;
 
 	i = 0;
-	// time = initialising_time();
 	pthread_mutex_init(&gen->write, NULL);
 	while (i < args->nmb_philos)
 	{
-		// all->philos[i].begin_all = time;
 		all->philos[i].args = args;
 		if (pthread_create(&all->philos[i].thread_philo, NULL,
 				&threads, &all->philos[i]) != 0)
@@ -93,19 +89,3 @@ int launch_threads(t_args *args, t_gen *gen, t_all *all)
 	while (all->philos->args->stop == 0);
 	return (0);
 }
-
-// 	test 							resultat attendu 												resultat obtenu
-//
-// ./philo 1 200 200 200 			philo 1 ne prend qu'une fourchette et meurt au bout de 200 ms -	OK
-// ./philo 2 800 200 200			personne ne meurt - 											OK
-// ./philo 5 800 200 200 		 	personne ne meurt - 											OK
-// ./philo 5 800 200 200 7 			la simulation s'arrete quand chaque philo a mange 7 fois - 		OK
-// ./philo 4 410 200 200 			personne ne meurt - 											1 meurs
-// ./philo 4 310 200 200 			un philo meurt - 												OK
-// ./philo 4 500 200 1.2 			argument invalide - 											OK
-// ./philo 4 0 200 200 				argument invalide - 											OK
-// ./philo 4 -500 200 200 			argument invalide - 											OK
-// ./philo 4 500 200 2147483647 	un philo meurt au bout de 500 ms - 								OK
-// ./philo 4 2147483647 200 200 	personne ne meurt - 											OK
-// ./philo 4 214748364732 200 200 	argument invalide - 											OK
-// ./philo 4 200 210 200 			un philo meurt, il faut afficher la mort avant 210 ms -  		OK
--- end_conditions.c
+++ end_conditions.c
@@ -6,7 +6,7 @@
 /*   By: tserdet <tserdet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 13:02:23 by tserdet           #+#    #+#             */
-/*   Updated: 2023/05/06 22:59:56 by tserdet          ###   ########.fr       */
+/*   Updated: 2023/05/06 23:13:39 by tserdet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ int	if_he_died(t_philos *philos)
 	if (get_chrono(philos->begin_all) - philos->log_eat >=
 		philos->args->ttd	)
 	{
-		//printf("Philo n.%d : %d - %ld = %ld >= %d ?\n", philos->id, get_chrono(philos->begin_all), philos->log_eat, get_chrono(philos->begin_all) - philos->log_eat, philos->args->ttd);
 		philos->args->stop = 1;
 		pthread_mutex_lock(philos->ptr_write);
 		printf("%dms %d died\n", get_chrono(philos->begin_all), philos->id + 1);
